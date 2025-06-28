a3271b6840c519db0f355806fe35a2c4bd6ea96a
Daan Vos de Wael <daan@vosdewael.com>
2025-06-28 15:51:14 +0200

    feat: add Sentry config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
+import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig: NextConfig = {};
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  disableLogger: true,
+  org: "",
+  project: "",
+  silent: !process.env.CI,
+  widenClientFileUpload: true,
+});
